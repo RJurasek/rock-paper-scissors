eb1f8cdda43bcdd51a3c9108d3fe08775334ff34
RJurasek <rd.jurasek@gmail.com>
2023-02-12 11:32:51 -0700

    evaluate playerinput and comp input + declare winner

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function getComputerChoice() {
-  let array = ["Rock", "Paper", "Scissors"];
+  let array = ["rock", "paper", "scissors"];
   let i = Math.floor(Math.random() * (2 - 0 + 1) + 0);
   return array[i];
 }
+ 
+const compChoice = getComputerChoice();
+const playerChoice = "ROCK";
 
-console.log(getComputerChoice())
+function playRound(playerChoice, compChoice) {
+    return (playerChoice.toLowerCase() == "rock" && compChoice == "scissors") ? "You Win! Rock beats scissors!"
+    : (playerChoice.toLowerCase() == "paper" && compChoice == "rock") ? "You Win! Paper beats rock!"
+    : (playerChoice.toLowerCase() == "scissors" && compChoice == "paper") ? "You Win! Scissors beats paper!"
+    : (playerChoice.toLowerCase() == "scissors" && compChoice == "rock") ? "You Lose! Rock beats scissors!"
+    : (playerChoice.toLowerCase() == "rock" && compChoice == "paper") ? "You Lose! Paper beats rock!"
+    : (playerChoice.toLowerCase() == "paper" && compChoice == "scissors") ? "You Lose! Scissors beats paper!"
+    : "I'ts a tie! try again!";
+}
+
+console.log(playRound(playerChoice, compChoice));
