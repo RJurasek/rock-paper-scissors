5473eb93923a38a49cc8f2e0ad2dd999e05b73fd
RJurasek <rd.jurasek@gmail.com>
2023-02-12 13:33:53 -0700

    finish rock-paper-scissors stage 1

--- script.js
+++ script.js
@@ -1,46 +1,58 @@
-function getComputerChoice() {
-  let array = ["rock", "paper", "scissors"];
-  let i = Math.floor(Math.random() * (2 - 0 + 1) + 0);
-  return array[i];
-}
 
-function getPlayerChoice() {
-    let playerInput = prompt("Chose rock, paper, or scissors");
-    let result = playerInput.toLowerCase();
-    return result;
-}
 
-const compChoice = getComputerChoice();
-const playerChoice = getPlayerChoice();
+function playRound(playerChoice, compChoice) {
+      if (playerChoice == "rock" && compChoice == "scissors") {
+        playerScore ++; 
+    } else if (playerChoice == "paper" && compChoice == "rock") {
+        playerScore++;
+    } else if (playerChoice == "scissors" && compChoice == "paper") { 
+        playerScore++; 
+    } else if (playerChoice == "scissors" && compChoice == "rock") {
+        compScore++;
+    } else if (playerChoice == "rock" && compChoice == "paper") {
+        compScore++;
+    } else if (playerChoice == "paper" && compChoice == "scissors") { 
+        compScore++;
+    } else if (playerChoice == compChoice) { 
+        console.log("tie"); 
+    } else { 
+        console.log("Enter valid selection! Refresh page.");
+    }
+} 
+
+function game() {
+    for (i = 0; i < 5; i++){
+        function getComputerChoice() {
+            let array = ["rock", "paper", "scissors"];
+            let i = Math.floor(Math.random() * (2 - 0 + 1) + 0);
+            return array[i];
+        }
+        
+        function getPlayerChoice() {
+        let playerInput = prompt("Chose rock, paper, or scissors");
+        let result = playerInput.toLowerCase();
+        return result;
+        }  
+
+        const compChoice = getComputerChoice();
+        const playerChoice = getPlayerChoice();
+
+        playRound(playerChoice, compChoice);
+        console.log(`Player:${playerScore}`, `Computer:${compScore}`);
+    }
+  if (playerScore > compScore){
+    return console.log(`You Win!! Score: ${playerScore} to ${compScore}!!`);
+  } else if (compScore > playerScore) {
+    return console.log(`You lost!! Score: ${playerScore} to ${compScore}!! try again.`);
+  } else {
+    return console.log(`It's a tie!! ${playerScore} to ${compScore}`)
+  }
+}
 
 let playerScore = 0;
 let compScore = 0;
 
-function playRound(playerChoice, compChoice) {
-    playerChoice.toLowerCase();
-      if (playerChoice == "rock" && compChoice == "scissors") {
-        playerScore ++;
-        return "You Win! Rock beats scissors!"; 
-    } else if (playerChoice == "paper" && compChoice == "rock") {
-        playerScore++;
-        return "You Win! Paper beats rock!";
-    } else if (playerChoice == "scissors" && compChoice == "paper") { 
-        playerScore++;
-        return "You Win! Scissors beats paper!"; 
-    } else if (playerChoice == "scissors" && compChoice == "rock") {
-        compScore++;
-        return "You Lose! Rock beats scissors!";
-    } else if (playerChoice == "rock" && compChoice == "paper") {
-        compScore++;
-        return "You Lose! Paper beats rock!";
-    } else if (playerChoice == "paper" && compChoice == "scissors") { 
-        compScore++;
-        return "You Lose! Scissors beats paper!";
-    } else if (playerChoice == compChoice) { 
-        return "I'ts a tie! try again!"; 
-    } else {
-        return "Enter valid selection! Refresh page.";
-    }
-} 
 
+
+game();
 
